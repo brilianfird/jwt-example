2c9dda037bd80508bebe73fcab229a7ef4193987
brilian.firdaus <brilian.firdaus@gdn-commerce.com>
2022-05-13 16:15:53 +0700

    add exception handler and change employee to resource

---
+++ src/main/java/com/brilianfird/jwtexample/controller/handler/ExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.brilianfird.jwtexample.controller.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandler extends ResponseEntityExceptionHandler {}
