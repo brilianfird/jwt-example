a4db38242397faf3a5dd2923275519133b3dd9e4
brilian.firdaus <brilian.firdaus@gdn-commerce.com>
2022-06-03 20:52:52 +0700

    add comment on JWE and refactor

--- src/test/java/com/brilianfird/jwtexample/Jose4j.java
+++ src/test/java/com/brilianfird/jwtexample/Jose4j.java
@@ -10,8 +10,6 @@ import org.jose4j.jwk.HttpsJwks;
 import org.jose4j.jwk.JsonWebKey;
 import org.jose4j.jwk.JsonWebKeySet;
 import org.jose4j.jwk.PublicJsonWebKey;
-import org.jose4j.jwk.RsaJsonWebKey;
-import org.jose4j.jwk.RsaJwkGenerator;
 import org.jose4j.jws.AlgorithmIdentifiers;
 import org.jose4j.jws.JsonWebSignature;
 import org.jose4j.jwt.JwtClaims;
@@ -25,7 +23,6 @@ import org.jose4j.lang.JoseException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.security.Key;
 import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
@@ -333,31 +330,43 @@ public class Jose4j {
 
   @Test
   public void JWE_ECDHES256() throws Exception {
+    // Determine signature algorithm and encryption algorithm
+    String alg = KeyManagementAlgorithmIdentifiers.ECDH_ES_A256KW;
+    String encryptionAlgorithm = ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256;
+
+    // Generate EC JWK
+    EllipticCurveJsonWebKey ecJWK = EcJwkGenerator.generateJwk(EllipticCurves.P256);
+
+    // Create
     JwtClaims jwtClaims = new JwtClaims();
     jwtClaims.setIssuer("https://codecurated.com");
     jwtClaims.setExpirationTimeMinutesInTheFuture(300);
     jwtClaims.setIssuedAtToNow();
     jwtClaims.setSubject("12345");
 
-    String alg = KeyManagementAlgorithmIdentifiers.ECDH_ES_A256KW;
-    String encryptionAlgorithm = ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256;
-
-    EllipticCurveJsonWebKey ecJWK = EcJwkGenerator.generateJwk(EllipticCurves.P256);
-
+    // Create JWE
     JsonWebEncryption jwe = new JsonWebEncryption();
     jwe.setPlaintext(jwtClaims.toJson());
+
+    // Set JWE's signature algorithm and encryption algorithm
     jwe.setAlgorithmHeaderValue(alg);
     jwe.setEncryptionMethodHeaderParameter(encryptionAlgorithm);
-    jwe.setKey(ecJWK.getKey());
+
+    // Unlike JWS, to create the JWE we use the public key
+    jwe.setKey(ecJWK.getPublicKey());
     String compactSerialization = jwe.getCompactSerialization();
     System.out.println(compactSerialization);
 
+    // Create JWT Consumer
     JwtConsumer jwtConsumer =
-            new JwtConsumerBuilder()
-                    .setVerificationKey(ecJWK.getPrivateKey())
-                    .setDecryptionKey(ecJWK.getPrivateKey())
-                    .setDisableRequireSignature().build();
+        new JwtConsumerBuilder()
+            // We set the private key as decryption key
+            .setDecryptionKey(ecJWK.getPrivateKey())
+            // JWE doesn't have signature, so we disable it
+            .setDisableRequireSignature()
+            .build();
 
+    // Get the JwtContext of the JWE
     JwtContext jwtContext = jwtConsumer.process(compactSerialization);
 
     System.out.println(jwtContext.getJwtClaims());
